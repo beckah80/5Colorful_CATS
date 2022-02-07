663788569cb207d79c8c67a9fda7e2671a4dd662
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-02-07 13:38:06 -0800

    Add project files.

---
+++ _5Colorful_CATS.py
@@ -0,0 +1,20 @@
+print("Hello, my friend1! I have a fun and adventurous game for you to play!  ")
+print("First, I will ask you a few questions and your answers will make the story!  ")
+print("Be sure that you press the enter key after every response, please. Are you ready?  ")
+print("Alright then, let's go!!  ")
+input("\nPress the enter key to continue...")
+
+Eye_color = input("\nWhat color are your eyes:  ")
+Favorite_color = input("\nWhat is your favorite color:  ")
+Lucky_number  = input("\nWhat is your lucky number:  ")
+Dream_vacation = input("\nWhere is your dream vacation:  ")
+Favorite_fish  = input("\nWhat is your favorite kind of fish:  ")
+
+print("\nLet's get going!!!  ")
+
+print("Once there were 5 colorful,",eye_color,"cats that lived together with their mean master, a mean old man.  ")
+
+
+
+
+
