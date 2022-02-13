c7e1e2a079946c480ce456b8b0c1312efbfee3a8
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-02-13 12:42:02 -0800

    CIS course project

--- _5Colorful_CATS.py
+++ _5Colorful_CATS.py
@@ -4,15 +4,36 @@ print("Be sure that you press the enter key after every response, please. Are yo
 print("Alright then, let's go!!  ")
 input("\nPress the enter key to continue...")
 
-Eye_color = input("\nWhat color are your eyes:  ")
-Favorite_color = input("\nWhat is your favorite color:  ")
-Lucky_number  = input("\nWhat is your lucky number:  ")
-Dream_vacation = input("\nWhere is your dream vacation:  ")
-Favorite_fish  = input("\nWhat is your favorite kind of fish:  ")
+eye_color = input("\nWhat color are your eyes:  ")
+middle_name = input("\nWhat is your middle name:  ")
+favorite_color = input("\nWhat is your favorite color:  ")
+lucky_number  = input("\nWhat is your lucky number:  ")
+dream_vacation = input("\nWhere is your dream vacation:  ")
+favorite_fish  = input("\nWhat is your favorite kind of fish:  ")
 
 print("\nLet's get going!!!  ")
 
-print("Once there were 5 colorful,",eye_color,"cats that lived together with their mean master, a mean old man.  ")
+print("Once there were 5 colorful,",eye_color,"cats that lived together with their master, a mean old man.  ")
+print("He would not ever let them have their,",favorite_fish,"to eat.   ")
+print("They were all very hungry and wanted to run away and go into the city where they new they could hunt and find,",favorite_fish,"to eat easily.  ")
+print("As these colorful felines are trying to decide when they should break-out, one of the cats,",middle_name,",yells out, we will leave at,",lucky_number,".  ")
+
+print("\nWhen it finally became,",lucky_number,"the 5 cats broke free and took off towards the city as fast as their little feet would move.  ")
+print("The whole time they were running,",middle_name,"kept screamin,I cannot wait to eat some,",favorite_fish,"!  ")
+print("\nAs they were coming around the corner, they could see a bowling alley coming up.  ")
+print("While running,",middle_name,"looked back and saw their master running after them.   ")
+print("They went into the bowling alley only to find there was a sushi bar ahead. ")
+print("\nThey ate and ate not thinking about the,",favorite_fish,"they were going after.  ")
+
+print("As the cats are coming out of the bowling alley,",middle_name,"screams out loudly that mean old man was catching up with them!  ")
+print("The 5,",eye_color,"cats see a trash can and they do not know what to do? ")
+print("\nThe poor cats longed to make it to,",dream_vacation,"and eat all the,",favorite_fish,"they could find.  ")
+print("The cat,",middle_name,"starts crying afraid that they would get caught.")
+print("As they came aroung the corner, they saw a sign that read,",dream_vacation,"straight ahead! ")
+print("As the cats thought about,",favorite_fish,"the cats change into the color of,",favorite_color,"helping them to escape")
+
+
+
 
 
 
