85f99c8c021aecb23f1d08fd59407e92e46f2ca6
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-02-19 08:52:31 -0800

    cats if/else statements

--- _5Colorful_CATS.py
+++ _5Colorful_CATS.py
@@ -13,26 +13,29 @@ favorite_fish  = input("\nWhat is your favorite kind of fish:  ")
 
 print("\nLet's get going!!!  ")
 
-print("Once there were 5 colorful,",eye_color,"cats that lived together with their master, a mean old man.  ")
-print("He would not ever let them have their,",favorite_fish,"to eat.   ")
+print("\nOnce there were 5 colorful,",eye_color,"cats that lived together with their master, a mean old man.  ")
+print("He would not ever let them have their",favorite_fish,"to eat.   ")
 print("They were all very hungry and wanted to run away and go into the city where they new they could hunt and find,",favorite_fish,"to eat easily.  ")
 print("As these colorful felines are trying to decide when they should break-out, one of the cats,",middle_name,",yells out, we will leave at,",lucky_number,".  ")
+print("When it finally became,",lucky_number, "the 5 cats broke free and took off towards the city as fast as their little feet would move.  ")
+print("The whole time they were running,",middle_name, "kept screamin,I cannot wait to eat some,",favorite_fish,"!  ")
+print("As they were coming around the corner, they could see a bowling alley coming up.  ")
+                                                                         
+goIntoAlley = input("\nShould the " + str(lucky_number) + "cats go into the bowling alley?  Type yes or no:  ")
 
+if goIntoAlley == "yes":
+    print("\nThey went into the bowling alley only to find there was a sushi bar ahead. ")
+    print("While they were standing inside,",middle_name,"says, yum it is a sushi bar just for us!  ")
+    print("Let's eat! The cats start eating the fish and fish eggs, forgetting all about the",favorite_fish,"they were going to hunt in the city.  ")
+    print("As the cats are coming out of the bowling alley,",middle_name,"screams out loudly that mean old man was catching up with them!  ")
+    print("As the,",eye_color,"cats are running fast still trying to loose the mean old man, they see a bunch of trashcans coming up  ")
+    
+else:
+    print("\nThe cats noticed that there was a sign ahead so they decided not to go inside the bowling alley. They continued to run.  ")
+    print("Then the cat,",middle_name,"looks back and let's out a loud squeel.  ")
+    print("The mean old man was catching up with the cats and they were hungry and afraid.")
 
-print("\nWhen it finally became,",lucky_number,"the 5 cats broke free and took off towards the city as fast as their little feet would move.  ")
-print("The whole time they were running,",middle_name,"kept screamin,I cannot wait to eat some,",favorite_fish,"!  ")
-print("\nAs they were coming around the corner, they could see a bowling alley coming up.  ")
-allowCats=input("\nShould the cats go inside the bowling alley?   ")
-
-print("While running,",middle_name,"looked back and saw their master running after them.   ")
-print("They went into the bowling alley only to find there was a sushi bar ahead. ")
-print("\nThey ate and ate not thinking about the,",favorite_fish,"they were going after.  ")
-print("As the cats are coming out of the bowling alley,",middle_name,"screams out loudly that mean old man was catching up with them!  ")
-print("The 5,",eye_color,"cats see a trash can and they do not know what to do? ")
-print("\nThe poor cats longed to make it to,",dream_vacation,"and eat all the,",favorite_fish,"they could find.  ")
-print("The cat,",middle_name,"starts crying afraid that they would get caught.")
-print("As they came aroung the corner, they saw a sign that read,",dream_vacation,"straight ahead! ")
-print("As the cats thought about,",favorite_fish,"the cats change into the color of,",favorite_color,"helping them to escape")
+hideInTrash = input("Should the cats hide in the trashcans?  Type yes or no:  ")
 
 
 
